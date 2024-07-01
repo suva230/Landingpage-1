0b180ab4ced6ea73e877b9a755c24a3dda174851
suva <testkoro2023@gmail.com>
2024-07-01 21:53:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let tl = gsap.timeline({
+    repeat: -1
+});
+
+tl
+.to(".imageContainer", {
+    ease: Expo.easeInOut,
+    width: "100%",
+    stagger: 2
+}, 'a')
+.to(".text h1", {
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: 0
+}, 'a')
+.to(".text h1", {
+    delay: 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%"
+}, 'a')
